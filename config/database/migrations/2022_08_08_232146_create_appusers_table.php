b8793b675ba6dcedb1da7b2480ad8d6626793b2b
Stalemilk Software <mvelimmachi@gmail.com>
2022-11-13 13:10:38 +0200

    initial commit

---
+++ config/database/migrations/2022_08_08_232146_create_appusers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAppusersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appusers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('surname');
+            $table->string('email');
+            $table->string('password');
+            $table->string('confirm_password')->default('');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appusers');
+    }
+}
